d89d27eb29c6e65ffedb41e1526c1f45cc4b003a
elederd <mjjg140698@gmail.com>
2024-07-27 03:13:04 -0400

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@ export default {
     token: process.env.TOKEN,
     clientId: "917110333640962100",
     color: "#00ff00",
-    replicateToken: "",
+    replicateToken: process.env.REPLICATE_API_TOKEN,
     model: "stability-ai/stable-diffusion:db21e45d3f7023abc2a46ee38a23973f6dce16bb082a930b0c49861f96d1e5bf" as any
 }
